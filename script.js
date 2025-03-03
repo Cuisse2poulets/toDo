6a79f52c6f752f9ca92d6426a2278e9951e77155
Cuisse2poulets <oggabooganiga@gmail.com>
2025-03-03 16:46:19 +0100

    toDo

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-var addToDoButton = document.getElementById('addToDo');
-var toDoContainer = document.getElementById('toDoContainer');
-var inputField = document.getElementById('inputField');
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
-addToDoButton.onclick = function(){
-    alert ('Vous avez cliquez sur +')
-    if(inputField.value != ""){
-        var paragraph =document.createElement('p')
+addToDoButton.onclick = function() {
+    if (inputField.value.trim() !== "") {  // Vérifier que le champ n'est pas vide
+        let paragraph = document.createElement('p'); 
+        paragraph.innerText = inputField.value;
+        paragraph.classList.add('paragraphe_style');
+
+        toDoContainer.appendChild(paragraph);
+        inputField.value = ""; 
+
+        paragraph.addEventListener('click', function() {
+            paragraph.classList.add('paragraph_click'); 
+        });
+
+        paragraph.addEventListener('dblclick', function() {
+            let newText = prompt("Modifier la tâche :", paragraph.innerText);
+            if (newText !== null && newText.trim() !== "") {
+                paragraph.innerText = newText;
+            }
+        });
+        
+    } else {
+        alert("Veuillez entrer une tâche !");
     }
-
-    paragraph.innerText = inputField.value;
-
-    paragraph.classList.add('paragraphe_style');
-
-    toDoContainer.appendChild(paragraph);
-
-    inputField.value = "";
-
-    paragraph.addEventListener('click',function(){
-        paragraph.classList.add('paragraph_click')
-    })
-
-    paragraph.addEventListener('dblclick',function(){
-        toDoContainer.remove(paragraph);
-    })
-}
+};
