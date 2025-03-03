9b6644ac15252cd9a722d5b84d556a5115470dc8
Cuisse2poulets <oggabooganiga@gmail.com>
2025-03-03 14:01:11 +0100

    toDo

---
+++ script.js
@@ -0,0 +1,26 @@
+var addToDoButton = document.getElementById('addToDo');
+var toDoContainer = document.getElementById('toDoContainer');
+var inputField = document.getElementById('inputField');
+
+addToDoButton.onclick = function(){
+    alert ('Vous avez cliquez sur +')
+    if(inputField.value != ""){
+        var paragraph =document.createElement('p')
+    }
+
+    paragraph.innerText = inputField.value;
+
+    paragraph.classList.add('paragraphe_style');
+
+    toDoContainer.appendChild(paragraph);
+
+    inputField.value = "";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.classList.add('paragraph_click')
+    })
+
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.remove(paragraph);
+    })
+}
